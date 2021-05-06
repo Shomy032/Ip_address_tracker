9932328e6e77a4760d0d853ad8386c682c5a6f78
Milos <milosmilic032@gmail.com>
2021-05-07 01:14:46 +0200

    final commit before upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById('btn');
 const form = document.getElementById('form');
+const alertt = document.getElementById('alert');
 
 const ip_text =  document.getElementById('ip_text');
 const locationn = document.getElementById('location');
@@ -24,8 +25,9 @@ window.addEventListener('load' , () =>{
 })
 
 form.addEventListener('submit' , (e) =>{
-  
+
    e.preventDefault()
+
 
   callApiSubmit()
 
@@ -36,10 +38,14 @@ const api_key = '1e15d3d7a81f43eaab37ab477be05b84';
 const IP_API_LINK = `https://ipgeolocation.abstractapi.com/v1/?api_key=${api_key}` ;
 // initial API call function 
 async function callApi(){
-const response = await fetch(IP_API_LINK);
-const data = await response.json();
+
+ const response = await fetch(IP_API_LINK);
+ const data = await response.json();
+ 
 
 current_ip = data.ip_address ;
+
+
 ip_text.innerText = current_ip ;
 
  current_city = data.region ;
@@ -61,6 +67,7 @@ setMap()
 }
 
 //setting up our map
+// this function is called in previos 2 functions
 function setMap(){
 const map_container = document.getElementById('map_container');
 const map_div = document.createElement('div');
@@ -86,9 +93,24 @@ async function callApiSubmit(){
 
   // storing what user submit
   let current_ip_submit = document.getElementById('input_ip').value;
+//
+ if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(current_ip_submit))
+  {
+    console.log('good ip');
+    alertt.style.display = 'none';
+  } else {
+    alertt.style.display = 'block';
+    alert("You have entered an invalid IP address!");
+    return
+  }
+
+
+
+//
   console.log(current_ip_submit);
   const IP_API_LINK_SUBMIT = `https://ipgeolocation.abstractapi.com/v1/?api_key=${api_key}&ip_address=${current_ip_submit}` ; // url for second call
 
+try{
   const response = await fetch(IP_API_LINK_SUBMIT);
   const data = await response.json();
 
@@ -96,6 +118,11 @@ async function callApiSubmit(){
   ip_text.innerText = current_ip ;
   
    current_city = data.region ;
+   if(current_city == null){
+    current_city = '????'
+   } else {
+    current_city = data.region ;
+   }
    current_country_code = data.country_code ;
    locationn.innerText = `${current_city} , ${current_country_code}`
   
@@ -106,12 +133,16 @@ async function callApiSubmit(){
   current_isp_name = data.connection.isp_name ;
   isp.innerText = current_isp_name;
 
-  latitude = data.latitude ;
-  longitude = data.longitude ;
+latitude = data.latitude ;
+longitude = data.longitude ;
 
    setNewMap();
 
   console.log('succes' ,data)
+} catch (err) {
+console.log( "error" , err)
+}
+
 }
 
 function setNewMap(){
